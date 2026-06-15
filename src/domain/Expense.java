be6808fd0458f7e400fe86e299a4c750355cef88
JashawnRogers <rogersjashawn20@gmail.com>
2026-06-14 21:55:12 -0700

    Added save and load from CSV file

--- src/domain/Expense.java
+++ src/domain/Expense.java
@@ -40,8 +40,40 @@ public class Expense {
         }
     }
 
-    public static Expense createNew(String description, BigDecimal amount, Category category, LocalDate date) {
-        return new Expense(description, amount, category, date);
+    public Expense(Long id, String description, BigDecimal amount, Category category, LocalDate date) {
+        if (id != null) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("An ID is required to map an existing expense to an Expense object");
+        }
+
+        if (description != null && !description.isBlank()) {
+            this.description = description;
+        } else {
+            throw new IllegalArgumentException("A description must be provided to create an expense.");
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
+            this.amount = amount;
+        } else {
+            throw new IllegalArgumentException("Amount must be greater than 0.");
+        }
+
+        if (Category.isValid(category.name())) {
+            this.category = category;
+        } else {
+            throw new IllegalArgumentException("A valid category is needed to create an expense.");
+        }
+
+        if (date != null) {
+            this.date = date;
+        } else {
+            throw new IllegalArgumentException("A date is required to create an expense.");
+        }
+    }
+
+    public static Expense createFromExisting(Long id, String description, BigDecimal amount, Category category, LocalDate date) {
+        return new Expense(id, description, amount, category, date);
     }
 
     @Override
--- src/persistence/ExpenseFileRepository.java
+++ src/persistence/ExpenseFileRepository.java
@@ -1,25 +1,26 @@
 package persistence;
 
+import domain.Category;
 import domain.Expense;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class ExpenseFileRepository {
     private final Map<Long, Expense> expensesMap;
+    private static final String CLASSPATH = "/Users/jashawnrogers/IdeaProjects/ExpenseTrackerCLI/src/files/";
 
     public ExpenseFileRepository(Map<Long, Expense> expensesMap) {
         this.expensesMap = expensesMap;
     }
 
     public PrintWriter createFile(String fileName) throws IOException {
-        FileWriter file = new FileWriter(
-                "/Users/jashawnrogers/IdeaProjects/ExpenseTrackerCLI/src/files/"
-                        + fileName.toLowerCase().trim());
+        FileWriter file = new FileWriter(CLASSPATH + fileName.toLowerCase().trim());
 
         return new PrintWriter(file);
     }
@@ -67,6 +68,37 @@ public class ExpenseFileRepository {
         } catch (IOException e) {
             System.err.println("Failed to initialize CSV file : " + e.getMessage());
         }
+    }
+
+    public void load(String fileName) {
+        String validatedFileName = fileName.toLowerCase().strip();
+
+        try (Stream<String> lines = Files.lines(Path.of(CLASSPATH + validatedFileName))){
+            List<List<String>> fileData = lines
+                    .map(line -> Arrays.asList(line.split(",")))
+                    .toList();
+
+            mapFileData(fileData);
+
+            System.out.println("File successfully loaded.");
+        } catch (IOException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+    }
+
+    private Map<Long, Expense> mapFileData(List<List<String>> fileData) {
+        Map<Long, Expense> expenses = new LinkedHashMap<>();
+        for (List<String> row : fileData) {
+            Long id = Long.parseLong(row.getFirst());
+            String description = row.get(1);
+            BigDecimal amount = BigDecimal.valueOf(Double.parseDouble(row.get(2)));
+            Category category = Category.valueOf(row.get(3));
+            LocalDate date = LocalDate.parse(row.getLast());
+
+            expenses.put(id ,Expense.createFromExisting(id, description, amount,category,date));
+        }
+
+        return expenses;
     }
 
 }
